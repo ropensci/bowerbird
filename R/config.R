beb0922a5215513f3f74b9d64464d840ec97005a
Ben Raymond <ben@theraymonds.org>
2017-06-18 15:14:59 +1000

    make global options attrs of config tibble

--- R/config.R
+++ R/config.R
@@ -1,7 +1,13 @@
 #' Load or initialize bowerbird configuration
 #'
 #' If \code{file} is provided, load that configuration. Otherwise initialize an empty configuration.
-#' @param file string: file name of configuration to load. All other parameters are ignored if \code{file} is provided.
+#' @param file string: file name of configuration to load. All other parameters are ignored if \code{file} is provided
+#' @param local_file_root string: location of data repository on local file system
+#' @param wget_flags string: default flags to be passed to wget. These are overridden on a per-data source basis if the data source defines its own wget_flags
+#' @param http_proxy string: URL of HTTP proxy to use e.g. 'http://your.proxy:8080' (NULL for no proxy)
+#' @param ftp_proxy string: URL of FTP proxy to use e.g. 'http://your.proxy:21' (NULL for no proxy)
+#' @param clobber numeric: 0=do not overwrite existing files, 1=overwrite if the remote file is newer than the local copy, 2=always overwrite existing files
+#' @param wait numeric: seconds to wait in between web calls. Some servers become unhappy if repeated calls are made too quickly from the same client address
 #' @param cf list: configuration, as returned by \code{bb_config}
 #' @param source tibble: external data source definition to add to the configuration
 #' @return configuration
@@ -20,20 +26,18 @@
 #' }
 #'
 #' @export
-bb_config <- function(file) {
+bb_config <- function(local_file_root,wget_flags="--progress=dot:giga",http_proxy=NULL,ftp_proxy=NULL,clobber=1,wait=0,file) {
     if (!missing(file)) {
         readRDS(file)
     } else {
-        list(global=tibble(
-                 wget_flags="--progress=dot:giga",
-                 http_proxy="",
-                 ftp_proxy="",
-                 local_file_root="",
-                 clobber=1,
-                 skip_downloads=FALSE,
-                 wait=0),
-             sources=tibble()
-             )
+        cf <- tibble()
+        attr(cf,"wget_flags") <- wget_flags
+        attr(cf,"http_proxy") <- http_proxy
+        attr(cf,"ftp_proxy") <- ftp_proxy
+        attr(cf,"local_file_root") <- local_file_root
+        attr(cf,"clobber") <- clobber
+        attr(cf,"wait") <- wait
+        cf
     }
 }
 
@@ -49,7 +53,6 @@ bb_save_config <- function(cf,file) {
 add <- function(cf,source) {
     ## need to do this by using global parms, if not overridden by source-specific values
     ## check that name is unique?
-    cf$sources <- dplyr::bind_rows(cf$sources,source)
-    cf
+    dplyr::bind_rows(cf,source)
 }
 
