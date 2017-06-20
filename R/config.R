8a084ae731deaee2306940d3c8aaf8d9d286f57b
Ben Raymond <ben@theraymonds.org>
2017-06-20 13:19:11 +1000

    export bb_attributes function

--- R/config.R
+++ R/config.R
@@ -1,6 +1,6 @@
 ## list of global flags, stored as attributes on the configuration tibble
 ## internal function
-bb_global_atts <- function() c("wget_default_flags","wget_global_flags","http_proxy","ftp_proxy","local_file_root","clobber")
+bb_global_atts <- function() c("wget_default_flags","wget_global_flags","http_proxy","ftp_proxy","local_file_root","clobber","skip_downloads")
 
 
 #' Initialize a bowerbird configuration
@@ -11,6 +11,7 @@ bb_global_atts <- function() c("wget_default_flags","wget_global_flags","http_pr
 #' @param http_proxy string: URL of HTTP proxy to use e.g. 'http://your.proxy:8080' (NULL for no proxy)
 #' @param ftp_proxy string: URL of FTP proxy to use e.g. 'http://your.proxy:21' (NULL for no proxy)
 #' @param clobber numeric: 0=do not overwrite existing files, 1=overwrite if the remote file is newer than the local copy, 2=always overwrite existing files. For data sources that use method 'wget', an appropriate flag will be added to the wget call according to the clobber setting ("--no-clobber" to not overwrite existing files, "--timestamping" to overwrite if the remote file is newer than the local copy)
+#' @param skip_downloads logical: if TRUE, \code{bb_sync} will do a dry run of the synchronisation process but without actually downloading files. For data sources using method bb_wget, this means that the wget calls will not be executed, so e.g. any recursion handled by wget itself will not be simulated
 #' @return configuration tibble
 #'
 #' @seealso \code{\link{bb_source}}
@@ -27,9 +28,10 @@ bb_global_atts <- function() c("wget_default_flags","wget_global_flags","http_pr
 #' }
 #'
 #' @export
-bb_config <- function(local_file_root,wget_default_flags=NULL,wget_global_flags="--restrict-file-names=windows --progress=dot:giga",http_proxy=NULL,ftp_proxy=NULL,clobber=1) {
+bb_config <- function(local_file_root,wget_default_flags=NULL,wget_global_flags="--restrict-file-names=windows --progress=dot:giga",http_proxy=NULL,ftp_proxy=NULL,clobber=1,skip_downloads=FALSE) {
     assert_that(is.string(local_file_root))
     assert_that(clobber %in% c(0,1,2))
+    assert_that(is.flag(skip_downloads))
     cf <- tibble()
     attr(cf,"wget_default_flags") <- wget_default_flags
     attr(cf,"wget_global_flags") <- wget_global_flags
@@ -37,7 +39,7 @@ bb_config <- function(local_file_root,wget_default_flags=NULL,wget_global_flags=
     attr(cf,"ftp_proxy") <- ftp_proxy
     attr(cf,"local_file_root") <- local_file_root
     attr(cf,"clobber") <- clobber
-    ## do we need user, password, skip_downloads?
+    attr(cf,"skip_downloads") <- skip_downloads
     cf
 }
 
@@ -59,12 +61,26 @@ add <- function(cf,source) {
     copy_bb_attributes(dplyr::bind_rows(cf,source),cf)
 }
 
-## helper functions to manage attributes
-bb_attributes <- function(obj) {
-    out <- attributes(obj)
+
+#' Returns a configuration object's bowerbird attributes
+#'
+#' @param cf tibble: configuration, as returned by \code{bb_config}
+#'
+#' @return named list
+#'
+#' @seealso \code{\link{bb_config}}
+#'
+#' @examples
+#' cf <- bb_config()
+#' bb_attributes(cf)
+#'
+#' @export
+bb_attributes <- function(cf) {
+    out <- attributes(cf)
     out[names(out) %in% bb_global_atts()]
 }
 
+## helper functions to manage attributes
 ## copy attributes
 copy_bb_attributes <- function(to,from) {
     attributes(to) <- c(attributes(to),bb_attributes(from))
