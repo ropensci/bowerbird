f1888d2ae2571a3494673366120360429ed8ea4c
Ben Raymond <ben@theraymonds.org>
2017-06-21 15:31:07 +1000

    refinements to earthdata handler

--- R/earthdata_handler.R
+++ R/earthdata_handler.R
@@ -12,7 +12,11 @@ earthdata_get <- function(data_source) {
     if (na_or_empty(data_source$user) || na_or_empty(data_source$password))
         stop(sprintf("Earthdata source \"%s\" requires user and password",data_source$name))
     cookies_file <- tempfile()
+    ## create this file
+    if (!file.exists(cookies_file)) cat("",file=cookies_file)
     dummy <- data_source
-    dummy$method_flags <- paste0("--http-user=",dummy$user," --http-password=",dummy$password," --load-cookies \"",cookies_file,"\" --save-cookies \"",cookies_file,"\" --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject \"index.html*\" -np -e robots=off ",dummy$method_flags," ",dummy$global_wget_flags)
+    dummy$method_flags <- paste0("--http-user=\"",dummy$user,"\" --http-password=\"",dummy$password,"\" --load-cookies \"",cookies_file,"\" --save-cookies \"",cookies_file,"\" --keep-session-cookies --no-check-certificate --auth-no-challenge -r --reject \"index.html*\" -np -e robots=off ",dummy$method_flags," ",dummy$wget_global_flags)
+    dummy$user <- as.character(NA)
+    dummy$password <- as.character(NA)
     bb_wget(dummy)
 }
