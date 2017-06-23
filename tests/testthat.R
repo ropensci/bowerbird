e9825183a2af0218166f4c68acd2a6e1840d7621
Ben Raymond <ben@theraymonds.org>
2017-06-24 09:22:29 +1000

    try installing wget if needed on windows prior to testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,11 @@ library(testthat)
 library(dplyr)
 library(bowerbird)
 
+if (.Platform$OS.type=="windows") {
+    tryCatch(wget_exe(),
+             error=function(e) {
+                 warning("At init of testing: could not find wget executable, installing.\n")
+                 install_wget()
+             })
+}
 test_check("bowerbird")
