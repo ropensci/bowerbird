0daf9c804402b9556d6d672e4895e405832ee543
Ben Raymond <ben@theraymonds.org>
2017-07-08 19:25:28 +1000

    use windows-friendly path in test

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -57,7 +57,7 @@ test_that("local directory looks right",{
         method=bb_wget,
         method_flags="",
         data_group="blah")
-    cf <- bb_config("~/some/local/path") %>% add(src)
-    expect_identical(data_source_dir(cf[1,]),"~/some/local/path/some.place.com/some/path/")
+    cf <- bb_config("/some/local/path") %>% add(src)
+    expect_identical(data_source_dir(cf[1,]),"/some/local/path/some.place.com/some/path/")
 })
 
