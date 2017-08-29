b7880e9750576a519b8726809929618ff005684b
Ben Raymond <ben.raymond@aad.gov.au>
2017-08-29 14:20:13 +1000

    fix oceandata calls to bb_wget

--- R/oceandata.R
+++ R/oceandata.R
@@ -50,11 +50,11 @@ bb_handler_oceandata <- function(config,verbose=FALSE,local_dir_only=FALSE) {
         ## sometimes this takes a couple of attempts!
         if (FALSE) {
             ## system2 code
-            myfiles <- bb_wget("https://oceandata.sci.gsfc.nasa.gov/search/file_search.cgi",paste0("-q --post-data=\"cksum=1&",method_flags,"\" -O -"),capture_stdout=TRUE)
+            myfiles <- bb_wget("https://oceandata.sci.gsfc.nasa.gov/search/file_search.cgi",paste0("-q --post-data=\"cksum=1&",method_flags,"\" -O -"),stdout=TRUE)
             if (is.null(attr(myfiles,"status")) || length(myfiles)>0) break
         } else {
             ## sys code, nb don't quote args, will break on unix
-            myfiles <- bb_wget("https://oceandata.sci.gsfc.nasa.gov/search/file_search.cgi",c("-q",paste0("--post-data=cksum=1&",method_flags),"-O","-"))
+            myfiles <- bb_wget("https://oceandata.sci.gsfc.nasa.gov/search/file_search.cgi",c("-q",paste0("--post-data=cksum=1&",method_flags),"-O","-"),capture_stdout=TRUE)
             if (myfiles$status==0) break
         }
         tries <- tries+1
