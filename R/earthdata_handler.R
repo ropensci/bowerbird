5c81cb5623318fd8a88f0be7565f62abb170c03e
Ben Raymond <ben@theraymonds.org>
2018-07-09 12:17:18 +1000

    earthdata handler to use rget

--- R/earthdata_handler.R
+++ R/earthdata_handler.R
@@ -40,11 +40,13 @@ bb_handler_earthdata <- function(...) {
 # @param config bb_config: a bowerbird configuration (as returned by \code{bb_config}) with a single data source
 # @param verbose logical: if TRUE, provide additional progress output
 # @param local_dir_only logical: if TRUE, just return the local directory into which files from this data source would be saved
-bb_handler_earthdata_inner <- function(config,verbose=FALSE,local_dir_only=FALSE,...) {
+# @param use_wget logical: TRUE use wget, FALSE use rget
+bb_handler_earthdata_inner <- function(config, verbose = FALSE, local_dir_only = FALSE, use_wget = FALSE, ...) {
     assert_that(is(config,"bb_config"))
     assert_that(nrow(bb_data_sources(config))==1)
     assert_that(is.flag(verbose),!is.na(verbose))
     assert_that(is.flag(local_dir_only),!is.na(local_dir_only))
+    assert_that(is.flag(use_wget), !is.na(use_wget))
 
     ## the earthdata-recommended way to use wget requires a .netrc file with username and password
     ## see e.g. https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+cURL+And+Wget
@@ -62,13 +64,25 @@ bb_handler_earthdata_inner <- function(config,verbose=FALSE,local_dir_only=FALSE
     ## create this file
     if (!file.exists(cookies_file)) cat("",file=cookies_file)
     on.exit(file.remove(cookies_file))
-    ## must use --auth-no-challenge else the server redirects to the html login page, rather than accepting the provided credentials
-    dummy$method <- list(list("bb_handler_wget",...,extra_flags=c("--http-user",dummy$user,"--http-password",dummy$password,"--auth-no-challenge","--load-cookies",cookies_file,"--save-cookies",cookies_file,"--keep-session-cookies"),reject="index.html*",robots_off=TRUE))
-    dummy$user <- NA_character_
-    dummy$password <- NA_character_
-    bb_data_sources(config) <- dummy
-    ## must make the wget call twice: first time it will authenticate, and write the cookies, but then redirect to the original page and wget won't go further because it knows it's already been there and doesn't want to get into an infinite loop
-    do.call(bb_handler_wget,c(list(config,verbose=verbose),dummy$method[[1]][-1]))
-    ## but the second time it will authenticate using the stored cookie and proceed with the recursion
-    do.call(bb_handler_wget,c(list(config,verbose=verbose),dummy$method[[1]][-1]))
+    ## must use --auth-no-challenge (force basic auth) else the server redirects to the html login page, rather than accepting the provided credentials
+    if (use_wget) {
+        dummy$method <- list(list("bb_handler_wget", ..., extra_flags = c("--http-user", dummy$user, "--http-password", dummy$password, "--auth-no-challenge", "--load-cookies", cookies_file, "--save-cookies", cookies_file, "--keep-session-cookies"), reject = "index.html*", robots_off = TRUE))
+        dummy$user <- NA_character_
+        dummy$password <- NA_character_
+        bb_data_sources(config) <- dummy
+        ## must make the wget call twice: first time it will authenticate, and write the cookies, but then redirect to the original page and wget won't go further because it knows it's already been there and doesn't want to get into an infinite loop
+        do.call(bb_handler_wget,c(list(config,verbose=verbose),dummy$method[[1]][-1]))
+        ## but the second time it will authenticate using the stored cookie and proceed with the recursion
+        do.call(bb_handler_wget,c(list(config,verbose=verbose),dummy$method[[1]][-1]))
+    } else {
+        this_att <- bb_settings(config)
+        my_curl_config <- build_curl_config(debug = FALSE, show_progress = FALSE, user = dummy$user, password = dummy$password)
+        ## and some more configs specifically for earthdata
+        my_curl_config$options$followlocation <- 1
+        my_curl_config$options$cookiefile <- cookies_file ## reads cookies from here
+        my_curl_config$options$cookiejar <- cookies_file ## saves cookies here
+        my_curl_config$options$httpauth <- 1L # force basic authentication, equivalent to wget's --auth-no-challenge flag
+        with_config(my_curl_config, do.call(bb_handler_rget, c(list(config, verbose = verbose), dummy$method[[1]][-1])))
+        ##with_config(my_curl_config, bb_rget("https://daacdata.apps.nsidc.org/pub/DATASETS/nsidc0192_seaice_trends_climo_v2/", level = 1, dry_run = TRUE, verbose = TRUE))
+    }
 }
